623b6eea50538fbbd018f6e617efe202ee07cb2a
Junaid <mohdjunaid0741@gmail.com>
2022-08-18 16:44:44 +0530

    my first commit

---
+++ New folder/Exception Handling/src/Chess.java
@@ -0,0 +1,68 @@
+import java.util.*;
+public class Chess {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		/* System.out.println("Enter the letter: ");
+	     String s = sc.nextLine();
+	        
+		 System.out.println("Enter the number: ");
+         int n = sc.nextInt();
+       
+        if((n == 1 || n == 3 || n == 5 || n == 7)&&(s.equals("A") || s.equals("C") || s.equals("E") || s.equals("G"))) {
+        	System.out.println("Black");
+        }
+        else {
+        	System.out.println("Invalid");
+        }
+		
+		System.out.println("Enter your number: ");
+		int n = sc.nextInt();
+		
+		System.out.println("Table for your number is: ");
+		for(int i=1; i<=10; i++) {
+			System.out.printf("%d X %d = %d\n",n,i,n*i);
+		}
+		
+		System.out.println("Enter your number");
+		int n = sc.nextInt();
+		
+		 boolean Prime=true;
+	     for(int i=2;i<n;i++)
+	     {
+	         if(n%i==0){
+	           Prime=false;
+	           break;
+	         }
+	         
+	     }
+	     
+	     if(Prime)
+	     {
+	         System.out.println("Prime");
+	     }
+	     else
+	     {
+	    	 System.out.println("Not prime");
+	     }
+		
+		System.out.println("Enter number: ");
+		int n = sc.nextInt();
+		
+		int n1 = 0, n2 = 1, n3;
+		 for(int i=2; i<=n; i++) {
+			 n3 = n1 + n2;
+			 n1 = n2;
+			 n2 = n3;
+		 }
+		 System.out.println(n1 + " " + n2);*/
+		
+		System.out.println("Enter number: ");
+		int n = sc.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			
+		}
+	}
+}
